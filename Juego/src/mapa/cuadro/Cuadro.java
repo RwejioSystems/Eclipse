8959a66205709d535cb337e404c8d2db8f06b0a9
Andres Hernandez Lara <andreshernandezlara@gmail.com>
2016-03-03 14:47:00 -0600

    uPDATE

--- Juego/src/mapa/cuadro/Cuadro.java
+++ Juego/src/mapa/cuadro/Cuadro.java
@@ -1,6 +1,20 @@
 package mapa.cuadro;
 
-public class Cuadro {
+import graficos.Pantalla;
+import graficos.Sprite;
+
+public abstract class Cuadro {
 	private int x;
 	private int y;
+	
+	public Sprite sprite;
+	public Cuadro(Sprite sprite){
+		this.sprite= sprite;
+	}
+	public void mostrar(int x, int y, Pantalla pantalla){
+	}
+	
+	public boolean solido(){
+			return false;
+	}
 }
