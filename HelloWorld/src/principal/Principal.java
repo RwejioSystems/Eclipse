f6e963b08d3fe09015c506a232237f0db8423fa6
Andres Hernandez Lara <andreshernandezlara@gmail.com>
2016-03-04 11:07:15 -0600

    Actualizacion

--- HelloWorld/src/principal/Principal.java
+++
@@ -1,8 +0,0 @@
-package principal;
-
-public class Principal extends Secundaria{
-	public static void main(String args[]){
-		Secundaria sc=new Secundaria();
-		System.out.println("Hello Beast");
-	}
-}
